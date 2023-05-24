e0d4d167996486173065efb3ec52d808ce96ca0c
adam98adam <adamzebr98@gmail.com>
2023-05-24 23:32:18 +0200

    add animation to information component

--- src/app/components/information/information.component.ts
+++ src/app/components/information/information.component.ts
@@ -1,10 +1,33 @@
+import {animate, state, style, transition, trigger } from '@angular/animations'
 import { Component } from '@angular/core';
+
+const fadeInOut = trigger('fadeInOut', [
+  state(
+    'open',
+     style({
+      opacity: 1
+     })
+  ),
+  state(
+    'close',
+     style({
+      opacity: 0
+     })
+  ),
+  transition('open => close', [animate('1s ease-out')]),
+  transition('close => open', [animate('1s ease-in')])
+])
 
 @Component({
   selector: 'information',
   templateUrl: './information.component.html',
-  styleUrls: ['./information.component.css']
+  styleUrls: ['./information.component.css'],
+  animations: [fadeInOut]
 })
 export class InformationComponent {
+  show: boolean = false;
 
+  fadeInOut() {
+    this.show = !this.show
+  }
 }
